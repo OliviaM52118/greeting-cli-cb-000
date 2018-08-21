e2bb548dd93f63c4ba8819850c9bbcf035a74f4b
Olivia Macedo <liom62313@gmail.com>
2018-08-21 00:29:59 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,6 @@
 # code the #greeting method here!
+def greeting(name_var = "Ruby Programmer")
+  puts "Hi! I'm HAL, what's your name?"
+  name_var = gets.strip
+  puts "Hello " + name_var + ". It's nice to meet you."
+end
